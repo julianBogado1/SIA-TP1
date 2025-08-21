759a45bcf4d2e16f36a860c14dff597407d6060a
SantiNG101 <santing101@gmail.com>
2025-08-21 18:13:24 -0300

    BFS algorithm implementation and merge with methods from other branches

--- src/main/java/org/sokoban/main/BFS.java
+++ src/main/java/org/sokoban/main/BFS.java
@@ -1,34 +1,61 @@
 package org.sokoban.main;
-
 import org.sokoban.models.Board;
-import org.sokoban.models.Node;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class BFS {
-    Queue<Board> frontier = new LinkedList<Board>();
-    Queue<Board> solution = new LinkedList<Board>();
+    Queue<Board> frontier = new LinkedList<>();
+    List<Board> solution = new ArrayList<>();
+
     public static void main(String[] args) {
+        BFS solver = new BFS();
+        List<Board> answer = solver.bfs();
 
-
-    }
-
-    private boolean nextStep(Board current){
-        if(current.isGoal()){
-            solution.add(current);
-            return true;
+        if(answer==null){
+            System.out.println("No solution found");
+            return;
         }
-        //miro frontera y se a donde moverme
-        boolean result = nextStep();
+
+        int step=0;
+        for(Board b : answer){
+            System.out.println("paso" + (step++));
+            System.out.println(b);
+        }
+
+
     }
 
-    private Queue<Board> bfs(){
+    private boolean nextStep(Board current, Board parent, List<Board> solutionPath) {
+        solution.add(current);
+
+        if(current.isSolution()){
+            solution = new ArrayList<>(solutionPath);
+        }
+
+        //miro frontera y se a donde moverme
+        for (Board move : current.getPossibleBoards()) {
+            if(!move.equals(parent)) {
+                frontier.add(move);
+            }
+        }
+
+        Board next = frontier.poll();
+        if(next == null){
+            return false;
+        }
+
+        List<Board> newPath = new ArrayList<>(solutionPath);
+
+        return nextStep(next, current, newPath);
+    }
+
+    private List<Board> bfs(){
         Board root = new Board();
         frontier.add(root);
 
-        if(nextStep(root)) return solution;
+        if(nextStep(root, null, new ArrayList<>())){
+            return solution;
+        }
         return null;
     }
 }
