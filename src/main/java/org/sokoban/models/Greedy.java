2ef1e2a42d6f9de67cab1dcb085c799e03062b4c
Sergio Smirnoff <ssmirnoff@itba.edu.ar>
2025-08-21 06:58:32 -0300

    fix merge error

---
+++ src/main/java/org/sokoban/models/Greedy.java
@@ -0,0 +1,33 @@
+package org.sokoban.models;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Greedy<T> {
+
+    private List<T> items;
+    private Comparator<T> comparator;
+    private Predicate<T> constraint;
+
+    public Greedy(List<T> items, Comparator<T> comparator, Predicate<T> constraint){
+        this.items = new ArrayList<>(items);
+        this.comparator = comparator;
+        this.constraint = constraint;
+    }
+
+    public List<T> solve() {
+        List<T> solution = new ArrayList<>();
+        items.sort(comparator);
+
+        for (T item : items) {
+            if (constraint.test(item)) {
+                solution.add(item);
+            }
+        }
+
+        return solution;
+    }
+    
+}
