45030dc2ffb064ee2e97f25f8b22e71949005150
SantiNG101 <santing101@gmail.com>
2025-08-26 16:51:29 -0300

    Frontier added to solution output

--- src/main/java/org/sokoban/main/BFS.java
+++ src/main/java/org/sokoban/main/BFS.java
@@ -33,6 +33,7 @@ public class BFS {
         try (PrintWriter writer = new PrintWriter(new FileWriter(solver.outputFile))) {
             writer.printf("%s se encontró solución. ", found ? "Sí" : "No");
             writer.printf("Nodos expandidos: %d. ", solver.expanded);
+            writer.printf("Frontier: %d. ", solver.frontier.size());
             writer.printf("Tiempo de ejecución: %d ms. ", elapsed);
             writer.println();
             
