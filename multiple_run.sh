89d6d2ec0b95c8853bd301eebb4c0f8268d73e1f
julianBogado1 <jbogado@itba.edu.ar>
2025-08-31 13:27:20 -0300

    Allow for mutltiple runs inside multiple executor

--- multiple_run.sh
+++ multiple_run.sh
@@ -2,11 +2,14 @@
 
 mvn clean package
 
-mvn exec:java -Dexec.mainClass="org.sokoban.main.BFS" > BFS_output.txt
-mvn exec:java -Dexec.mainClass="org.sokoban.main.DFS" > DFS_output.txt
-mvn exec:java -Dexec.mainClass="org.sokoban.main.IDDFS" > IDDFS_output.txt
+for((i=0; i<$1; i++))
+do
+  mvn exec:java -Dexec.mainClass="org.sokoban.main.BFS" > outputs/BFS_output"$i".txt
+  mvn exec:java -Dexec.mainClass="org.sokoban.main.DFS" > outputs/DFS_output"$i".txt
+  mvn exec:java -Dexec.mainClass="org.sokoban.main.IDDFS" > outputs/IDDFS_output"$i".txt
 
-mvn exec:java -Dexec.mainClass="org.sokoban.main.Greedy" -Dexec.args="h1" > Greedy_h1_output.txt
-mvn exec:java -Dexec.mainClass="org.sokoban.main.Greedy" -Dexec.args="h2" > Greedy_h2_output.txt
-mvn exec:java -Dexec.mainClass="org.sokoban.main.AStar" -Dexec.args="h1" > AStar_h1_output.txt
-mvn exec:java -Dexec.mainClass="org.sokoban.main.AStar" -Dexec.args="h2" > AStar_h2_output.txt
+  mvn exec:java -Dexec.mainClass="org.sokoban.main.Greedy" -Dexec.args="h1" > outputs/Greedy_h1_output"$i".txt
+  mvn exec:java -Dexec.mainClass="org.sokoban.main.Greedy" -Dexec.args="h2" > outputs/Greedy_h2_output"$i".txt
+  mvn exec:java -Dexec.mainClass="org.sokoban.main.AStar" -Dexec.args="h1" > outputs/AStar_h1_output"$i".txt
+  mvn exec:java -Dexec.mainClass="org.sokoban.main.AStar" -Dexec.args="h2" > outputs/AStar_h2_output"$i".txt
+done
